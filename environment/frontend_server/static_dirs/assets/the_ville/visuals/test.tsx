6fbc3c45de1af3fd705aaaf3d7903bfec7cb1025
Turing's Cat <deanyuton@gmail.com>
2023-09-17 23:40:10 +0800

    update movements

---
+++ environment/frontend_server/static_dirs/assets/the_ville/visuals/test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="test" tilewidth="32" tileheight="32" tilecount="352" columns="22">
+ <image source="the_ville2.png" width="719" height="513"/>
+</tileset>
